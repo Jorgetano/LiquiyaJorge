ffa0d46bcf57459ac53363ad526f2f6107588664
Jorge Tano <jorgetano89@gmail.com>
2024-06-06 20:14:45 -0500

    actualizacion de repo

--- app/src/main/java/com/example/liquiya/Menu.java
+++ app/src/main/java/com/example/liquiya/Menu.java
@@ -1,18 +1,47 @@
 package com.example.liquiya;
 
 import android.os.Bundle;
-import android.view.View;
+
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Menu extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu);
+
+        // Inicializar Firebase Database
+        mDatabase  = FirebaseDatabase.getInstance().getReference();
+
+        mDatabase.child("Users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    DataSnapshot dataSnapchat ;
+                    String nombre = snapshot.child("nombre").getValue().toString();
+                    String apellido = snapshot.child("apellido").getValue().toString();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        // Leer datos desde Firebase
+
     }
 }
--- app/src/main/java/com/example/liquiya/restablecerContrasena.java
+++ app/src/main/java/com/example/liquiya/restablecerContrasena.java
@@ -1,11 +1,8 @@
 package com.example.liquiya;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
@@ -14,8 +11,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class restablecerContrasena extends AppCompatActivity {
@@ -36,58 +31,39 @@ public class restablecerContrasena extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         correoRecuperar = findViewById(R.id.CorreoRecuperar); // Obtener referencia al EditText
 
-        findViewById(R.id.BRecuperar).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = correoRecuperar.getText().toString().trim();
+        findViewById(R.id.BRecuperar).setOnClickListener(v -> {
+            String email = correoRecuperar.getText().toString().trim();
 
-                if (!email.isEmpty()) {
-                    mAuth.sendPasswordResetEmail(email)
-                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(Task<Void> task) {
-                                    if (task.isSuccessful()) {
-                                        // Mostrar diálogo modal de éxito
-                                        AlertDialog.Builder builder = new AlertDialog.Builder(restablecerContrasena.this);
-                                        builder.setTitle("Correo Enviado");
-                                        builder.setMessage("Se ha enviado un correo para restablecer la contraseña.");
-                                        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
-                                            @Override
-                                            public void onClick(DialogInterface dialog, int which) {
-                                                dialog.dismiss();
-                                                startActivity(new Intent(restablecerContrasena.this, MainActivity.class));
+            if (!email.isEmpty()) {
+                mAuth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                // Mostrar diálogo modal de éxito
+                                AlertDialog.Builder builder = new AlertDialog.Builder(restablecerContrasena.this);
+                                builder.setTitle("Correo Enviado");
+                                builder.setMessage("Se ha enviado un correo para restablecer la contraseña.");
+                                builder.setPositiveButton("Aceptar", (dialog, which) -> {
+                                    dialog.dismiss();
+                                    startActivity(new Intent(restablecerContrasena.this, MainActivity.class));
 
-                                            }
-                                        });
-                                        builder.show();
-                                    } else {
-                                        // Mostrar diálogo modal de error
-                                        AlertDialog.Builder builder = new AlertDialog.Builder(restablecerContrasena.this);
-                                        builder.setTitle("Error");
-                                        builder.setMessage("Error al enviar el correo de restablecimiento.");
-                                        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
-                                            @Override
-                                            public void onClick(DialogInterface dialog, int which) {
-                                                dialog.dismiss();
-                                            }
-                                        });
-                                        builder.show();
-                                    }
-                                }
-                            });
-                } else {
-                    // Mostrar diálogo modal para ingresar correo electrónico
-                    AlertDialog.Builder builder = new AlertDialog.Builder(restablecerContrasena.this);
-                    builder.setTitle("Correo Electrónico Vacío");
-                    builder.setMessage("Ingresa tu correo electrónico.");
-                    builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                        }
-                    });
-                    builder.show();
-                }
+                                });
+                                builder.show();
+                            } else {
+                                // Mostrar diálogo modal de error
+                                AlertDialog.Builder builder = new AlertDialog.Builder(restablecerContrasena.this);
+                                builder.setTitle("Error");
+                                builder.setMessage("Error al enviar el correo de restablecimiento.");
+                                builder.setPositiveButton("Aceptar", (dialog, which) -> dialog.dismiss());
+                                builder.show();
+                            }
+                        });
+            } else {
+                // Mostrar diálogo modal para ingresar correo electrónico
+                AlertDialog.Builder builder = new AlertDialog.Builder(restablecerContrasena.this);
+                builder.setTitle("Correo Electrónico Vacío");
+                builder.setMessage("Ingresa tu correo electrónico.");
+                builder.setPositiveButton("Aceptar", (dialog, which) -> dialog.dismiss());
+                builder.show();
             }
         });
     }
